7144a8343f86ef30a89f2c11960a5666d0ad879d
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-02 15:41:24 +0500

    2m

--- List02.py
+++ List02.py
@@ -1,4 +1,5 @@
 def main(list1):
+    
     """
     A list of several elements is given. Return the first item.
     Args:
@@ -6,4 +7,4 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    return list1[0]
