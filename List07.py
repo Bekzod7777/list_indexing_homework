77e51b8073ed6507916247da4d9de8014d26cd06
Bekzod <bekzodmirsaidov91@gmail.com>
2022-04-02 16:16:45 +0500

    7m

--- List07.py
+++ List07.py
@@ -1,4 +1,15 @@
 def main(list1):
+    if list1[0]==0:
+        list1[0]=False
+    if list1[1]==0:
+        list1[1]=False
+    if list1[2]==0:
+        list1[2]=False
+
+    if list1[3]==0:
+        list1[3]=False
+    if list1[4]==0:
+        list1[4]=False
     """
     A list of units and zeros with a length of five is given. Replace zero with False.
     Args:
@@ -6,4 +17,6 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    return list1
+
+    
